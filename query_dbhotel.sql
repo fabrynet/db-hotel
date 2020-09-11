dfc40b2f74a4311ca65a0dd3db041eccd16793e0
Fabrizio <fabrynet.bianco@gmail.com>
2020-09-11 11:44:30 +0200

    Query GROUP BY

--- query_dbhotel.sql
+++ query_dbhotel.sql
@@ -38,4 +38,36 @@ WHERE document_type LIKE 'Driver%'
 SELECT SUM(beds) AS posti_letto_totali
 FROM stanze;
 
+-- GROUP BY
+--     • Conta gli ospiti raggruppandoli per anno di nascita
+SELECT YEAR(date_of_birth) AS 'anno_di_nascita', COUNT(*) AS 'numero_ospiti'
+FROM ospiti
+GROUP BY YEAR(date_of_birth);
+
+--     • Somma i prezzi dei pagamenti raggruppandoli per status
+SELECT status, SUM(price) AS 'somma_pagamenti'
+FROM pagamenti
+GROUP BY status;
+
+--     • Conta quante volte è stata prenotata ogni stanza
+SELECT stanza_id, COUNT(*) AS 'numero_prenotazioni'
+FROM prenotazioni
+GROUP BY stanza_id;
+
+--     • Fai una analisi per vedere se ci sono ore in cui le prenotazioni sono più frequenti
+SELECT HOUR(created_at) AS 'ora_prenotazione', COUNT(created_at) AS 'numero_prenotazioni'
+FROM prenotazioni
+GROUP BY HOUR(created_at)
+ORDER BY COUNT(created_at) DESC;
+
+-- JOIN
+--     • Come si chiamano gli ospiti che hanno fatto più di due prenotazioni?
+
+
+--     • Stampare tutti gli ospiti per ogni prenotazione
+--     • Stampare Nome, Cognome, Prezzo e Pagante per tutte le prenotazioni fatte a Maggio 2018
+--     • Fai la somma di tutti i prezzi delle prenotazioni per le stanze del primo piano
+--     • Prendi i dati di fatturazione per la prenotazione con id=7
+--     • Le stanze sono state tutte prenotate almeno una volta? (Visualizzare le stanze non ancora prenotate)
+
 -- NOTE: e' sufficiente caricare un file di testo
